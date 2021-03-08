0d58b0bdc44d6bd1ae6d5724429b027c5c077fb4
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-08 02:09:36 -0500

    commit tree 5f83a4f064f94890343cbc504ff052b644032c92

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,28 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+
+  return(
+    <div className={'review-list'} style={{display: 'block', textAlign: 'center'}}>
+      <ul>
+        {reviews.map(makeReview)}
+      </ul>
+    </div>
+  )
+}
+
+const makeReview = ({display_title, headline, byline, link}) => {
+  return (
+  <li className="review" key={display_title + ` ` + byline}>
+    <a href={link.url}>{headline}</a>
+  </li>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+
+export default MovieReviews
